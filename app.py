fa6108ff47756bc8908c527c2f1728e9cbb2fcfe
aqsaaQaazi <aqsaakqaazi@gmail.com>
2025-04-16 01:47:12 +0500

    Implement encryption, decryption, and passkey hashing for secure data storage

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
-print("hello");
+# Imports
+import streamlit as st;
+import hashlib;
+from cryptography.fernet import Fernet;
+
+
+# Production 
+# Keys
+
+KEY = Fernet.generate_key();
+cipher = Fernet(KEY);
+
+# dict for storage
+stored_data: dict = {};
+failed_attempts: int = 0;
+
+# hashing Function;
+def hash_passkey(passkey):
+    return hashlib.sha256(passkey.encode()).hexdigest();
+
+# Fernet Encrypt
+def encrypt_data(text, passkey):
+    return cipher.encrypt(text.encode()).decode()
+
+# Fernet Decrypt
+def decrypt_data(encrypted_text, passkey):
+    global failed_attempts
+    hashed_passkey = hash_passkey(passkey)
+    
+# passkey validation
+    for key, value in stored_data.items():
+        if value["encrypted_text"] == encrypted_text and value["passkey"] == hashed_passkey:
+            failed_attempts = 0;
+            return cipher.decrypt(encrypted_text.encode()).decode;
+    failed_attempts += 1;
+    return None;
